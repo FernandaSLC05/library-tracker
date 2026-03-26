183ce6828b0f7901073e84fe8ed344f671485ebf
Fernanda Santa Lucia Corsi <fernandasantaluciac@gmail.com>
2026-03-26 07:10:27 -0300

    Atualizando nome do projeto

--- library-tracker/src/main/java/com/fernanda/library/BooksTrackerApplication.java
+++ library-tracker/src/main/java/com/fernanda/library/BooksTrackerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LibraryTrackerApplication {
+public class BooksTrackerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LibraryTrackerApplication.class, args);
+		SpringApplication.run(BooksTrackerApplication.class, args);
 	}
 
 }
